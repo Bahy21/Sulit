65bed43adf5bcb182560d93a18bf9619f3d388cf
tarek fouda <tarekfouda0@gmail.com>
2023-02-13 21:05:11 +0200

    home-profile(pages)-notification

--- lib/features/base/presentation/pages/wishlist/wishlist.dart
+++ lib/features/base/presentation/pages/wishlist/wishlist.dart
@@ -1,4 +1,5 @@
-part of'wishlist_imports.dart';
+part of 'wishlist_imports.dart';
+
 class Wishlist extends StatefulWidget {
   const Wishlist({Key? key}) : super(key: key);
 
@@ -10,18 +11,16 @@ class _WishlistState extends State<Wishlist> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const DefaultAppBar(title: "WishList", showBack: true),
-        body:GridView(
-          padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 16).r,
-          gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.r,
-              mainAxisSpacing: 8.r,
-              childAspectRatio: 8 / 10),
-          children: List.generate(
-              11,
-                  (index) => const BuildWishlistItem()),
-        ),
+      appBar: const DefaultAppBar(title: "WishList", showBack: true),
+      body: GridView(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16).r,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.r,
+            mainAxisSpacing: 8.r,
+            childAspectRatio: 8 / 10),
+        children: List.generate(11, (index) => const BuildWishlistItem()),
+      ),
     );
   }
 }
