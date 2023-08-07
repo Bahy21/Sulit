ba66302d6e0f8703d31134238801b3b194d18cd5
tarek fouda <tarekfouda1994@gmail.com>
2023-08-07 14:07:09 +0300

    address shimmer

---
+++ lib/features/user/addresses/presentation/pages/addresses/widgets/build_add_new_address_card_shimmer.dart
@@ -0,0 +1,27 @@
+part of 'addresses_widgets_imports.dart';
+class BuildAddNewAddressCardShimmer extends StatelessWidget {
+  const BuildAddNewAddressCardShimmer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal:Dimens.dp20, vertical: Dimens.dp10),
+      padding: const EdgeInsets.all(Dimens.dp15),
+      decoration: BoxDecoration(
+        borderRadius: Dimens.borderRadius10PX,
+        color: context.colors.white,
+        border: Border.all(color: context.colors.greyWhite),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const BuildShimmerView(
+            height: 7.5,
+            width: 50,
+          ),
+          BuildShimmerView(child: Icon(Icons.add_circle, color: context.colors.grey)),
+        ],
+      ),
+    );
+  }
+}
